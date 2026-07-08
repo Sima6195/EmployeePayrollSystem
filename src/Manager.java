dfed38fe3772ea91e252dc03966af99a7887dc70
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-07-08 12:30:30 +0200

    manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,2 +1,20 @@
-public class Manager {
+public class Manager extends Employee {
+    private double bonus;
+    private double allowance;
+
+    public Manager(String employeeName,
+                     String employeeSurname,
+                     String employeeNumber,
+                     double baseSalary) {
+
+        super(employeeName, employeeSurname, employeeNumber, "Manager", baseSalary);
+
+        this.bonus = baseSalary;
+        this.allowance = baseSalary;
+    }
+
+    public void calculateSalary() {
+        double totalSalary = getBasicSalary() + bonus + allowance;
+        setSalary(totalSalary);
+    }
 }
