277b3e445e2eaa5e0246a24fbf19227f36e1eb40
Dineo <Dineo.Makofane@ext.shaper.co.za>
2026-07-08 16:09:42 +0200

    Fixed the Menu list

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,8 @@ public class Main {
         System.out.println("1. Add Employee");
         System.out.println("2. View Employees");
         System.out.println("3. Search Employee");
-        System.out.println("4. Calculate Payroll");
-        System.out.println("5. Remove Employee");
-        System.out.println("6. Exit");
+        System.out.println("4. Remove Employee");
+        System.out.println("5. Exit");
         System.out.print("\nEnter your choice: ");
 
         int choice = scanner.nextInt();
