41da3fd11b1807d50cce5195a77cfee186607232
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-07-08 12:29:23 +0200

    Developer

--- src/Developer.java
+++ src/Developer.java
@@ -1,2 +1,19 @@
-public class Developer {
+public class Developer extends Employee {
+
+    private double bonus;
+
+    public Developer(String employeeName,
+                  String employeeSurname,
+                  String employeeNumber,
+                  double baseSalary) {
+
+        super(employeeName, employeeSurname, employeeNumber, "Developer", baseSalary);
+
+        this.bonus = baseSalary;
+    }
+
+    public void calculateSalary() {
+        double totalSalary = getBasicSalary() + bonus;
+        setSalary(totalSalary);
+    }
 }
